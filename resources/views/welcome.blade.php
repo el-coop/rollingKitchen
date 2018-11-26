547b808fb8ed4f8ca4c3b6a8d6c95cfd00c4055d
aliciaderuijter <aliciaderuijter@gmail.com>
2018-11-26 13:30:47 -0600

    committing homepage from old branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,60 @@
-@extends('layouts.dashboard')
+@extends('layouts.site')
 
 @section('content')
-	<h1>hello world</h1>
+
+    <section class="section">
+
+
+        <div class="columns">
+            <div class="column">
+
+                <div class="card is-flex" style="flex-direction: column; height: 100%">
+                    <div class="card-content" style="flex: 1">
+                        <p class="title">
+                            Klik op aanmelden als je in 2017 niet hebt meegedaan aan Het Weekend van de Rollende keukens.
+                        </p>
+
+                    </div>
+
+                    <footer class="card-footer">
+                        <button class="card-footer-item button is-dark is-size-3">
+
+                            Aanmelden
+
+                        </button>
+                    </footer>
+                </div>
+            </div>
+            <div class="column">
+
+
+                <div class="card is-flex" style="flex-direction: column; height: 100%">
+                    <div class="card-content" style="flex: 1">
+                        <p class="title">
+                            Klik op inloggen als je in 2017 ook al hebt meegedaan aan Het Weekend van de Rollende Keukens.
+                        </p>
+
+                    </div>
+
+                    <footer class="card-footer">
+                        <button class="card-footer-item button is-dark is-size-3">
+
+                            Inloggen
+
+                        </button>
+                    </footer>
+                </div>
+            </div>
+            <div class="column">
+
+                <figure class="image is-square">
+
+                    <img src="/images/kreeft.png" alt="kreeft">
+
+
+                </figure>
+            </div>
+        </div>
+
+    </section>
 @endsection
