8dbad72982d1ba738337c112052ccdec6678f984
Nur Bar <LCD344@yahoo.com>
2018-12-09 12:40:45 -0600

    changes to table

--- database/migrations/2018_11_25_153456_create_pdfs_table.php
+++ database/migrations/2018_11_25_153456_create_pdfs_table.php
@@ -5,26 +5,27 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreatePdfsTable extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up() {
-        Schema::create('pdfs', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('file');
-            $table->string('name');
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down() {
-        Schema::dropIfExists('pdfs');
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('pdfs', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('file');
+			$table->string('name');
+			$table->integer('show');
+			$table->timestamps();
+		});
+	}
+	
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('pdfs');
+	}
 }
