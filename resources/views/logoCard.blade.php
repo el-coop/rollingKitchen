4fbbf6fd60b72315e593a2c6ed5e69d640542b02
aliciaderuijter <aliciaderuijter@gmail.com>
2018-12-09 22:39:30 -0600

    added new logo to login page

--- resources/views/logoCard.blade.php
+++ resources/views/logoCard.blade.php
@@ -1,7 +1,7 @@
 <div class="column">
 	<div class="h-100 is-flex has-content-justified-center has-items-aligned-center">
 		<figure class="image is-square" style="width: 100%">
-			<img src="{{ asset('images/logo.png') }}" alt="@lang('global.title')">
+			<img src="{{ asset('images/awesomeKreeft.jpg') }}" alt="@lang('global.title')">
 		</figure>
 	</div>
 </div>
