2775853f19d770f3da13b0593e4b78b5a6caf94c
Nur Bar <LCD344@yahoo.com>
2018-12-10 18:32:59 -0600

    JS part done

--- resources/js/Components/Form/InvoiceField.vue
+++ resources/js/Components/Form/InvoiceField.vue
@@ -3,7 +3,8 @@
 		<div class="box">
 			<h5 class="title is-5" v-text="field.label"></h5>
 			<div class="columns is-mobile">
-				<div class="column" :class="{'is-2' : header !== 'item'}" v-for="header in headers">
+				<div class="column"
+					 :class="headerClass(header)" v-for="header in headers">
 					<h6 class="title is-6" v-text="$translations[header]"></h6>
 				</div>
 				<div class="column is-2"></div>
@@ -11,12 +12,15 @@
 			<invoice-line v-for="(entry, index) in values" :key="`{entry.uniqe_id}_${index}`" :index="index"
 						  :name="field.name"
 						  v-model="values[index]"
+						  :individual-tax="field.individualTax || false"
+						  :tax-options="field.taxOptions"
 						  :options="field.options"
 						  @total="updateTotal(index,$event)"
 						  @remove="remove(index)"></invoice-line>
-			<div class="columns is-mobile">
+			<div class="columns is-mobile" v-if="!(field.individualTax || false)">
 				<div class="column" :class="{'is-2' : header !== 'item'}" v-for="header in headers">
-					<span v-if="header === 'unitPrice'" v-text="$translations.vat"></span>
+					<span v-if="header === 'unitPrice'"
+						  v-text="$translations.vat"></span>
 					<div v-if="header === 'item'" class="select is-fullwidth">
 						<select name="tax" v-model="tax">
 							<option v-for="(taxLabel,taxValue) in field.taxOptions" :value="taxValue" v-text="taxLabel">
@@ -28,7 +32,7 @@
 				<div class="column is-2"></div>
 			</div>
 			<div class="columns is-mobile">
-				<div class="column" :class="{'is-2' : header !== 'item'}" v-for="header in headers">
+				<div class="column" :class="headerClass(header)" v-for="header in headers">
 					<span v-if="header === 'item'" v-text="$translations.total"></span>
 					<span v-if="header === 'total'" v-text="localNumber(totalSum * (1 + tax/100))"></span>
 				</div>
@@ -63,11 +67,19 @@
 				}];
 			}
 
+			const headers = ['quantity', 'unitPrice', 'item', 'total'];
+			let tax = 21;
+
+			if (this.field.individualTax || false) {
+				headers.splice(-2, 0, 'vat');
+				tax = 0;
+			}
+
 			return {
 				values,
-				headers: ['quantity', 'unitPrice', 'item', 'total'],
+				headers: headers,
 				sum: [0],
-				tax: 21
+				tax
 			}
 		},
 
@@ -86,6 +98,18 @@
 			},
 			updateTotal(index, payload) {
 				this.sum.splice(index, 1, payload);
+			},
+
+			headerClass(header) {
+				if (header === 'item') {
+					return {};
+				}
+
+				if (header !== 'total' || !(this.field.individualTax || false)) {
+					return 'is-2';
+				}
+
+				return 'is-1';
 			}
 		},
 
@@ -104,9 +128,10 @@
 	.box {
 		overflow: auto;
 
-		.column{
+		.column {
 			min-width: 150px;
-			&.is-2 {
+
+			&.is-2, &.is-1 {
 				min-width: 75px;
 			}
 		}
