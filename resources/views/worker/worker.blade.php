36badb350c2f58def42ed0c5a2d51247be643fe0
Cucurrucho <adam.bar91@gmail.com>
2019-01-28 10:40:07 -0600

    fixed datatable route and added datatable test

--- resources/views/worker/worker.blade.php
+++ resources/views/worker/worker.blade.php
@@ -7,8 +7,10 @@
         <tab label="@lang('worker/worker.profile')">@include('worker.profile')</tab>
         <tab label="@lang('worker/worker.shifts')"></tab>
         <tab label="@lang('worker/worker.workedHours')"></tab>
-        @if(Auth::user()->user->isSupervisor())
-            <tab label="@lang('worker/supervisor.manageWorkers')">@include('worker.supervisor.manageWorkplaces')</tab>
+        @if(Auth::user()->user_type == \App\Models\Worker::class)
+            @if(Auth::user()->user->isSupervisor())
+                <tab label="@lang('worker/supervisor.manageWorkers')">@include('worker.supervisor.manageWorkplaces')</tab>
+            @endif
         @endif
     </tabs>
 @endsection
