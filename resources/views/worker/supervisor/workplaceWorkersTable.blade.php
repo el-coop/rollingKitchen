8531422de476436db5b37a1e89f40ae58a0d3a81
Nur Bar <LCD344@yahoo.com>
2019-02-07 16:34:01 -0200

    Made better controller on supervisor tables

--- resources/views/worker/supervisor/workplaceWorkersTable.blade.php
+++ resources/views/worker/supervisor/workplaceWorkersTable.blade.php
@@ -1,20 +1,20 @@
 @component('components.nonConfigDatatable', [
-    'table' => $workersTable,
+    'attribute' => 'workersForSupervisor',
     'fields' => $workersTable['fields'],
-    'url' => 'supervisorDatatable',
+    'url' => "/supervisorDatatable/{$workplace->id}"
 ])
-    @slot('deleteButton', true)
-    @slot('buttons')
-        <button class="button is-light" @click="actions.newObjectForm">@lang('vue.add')</button>
-    @endslot
-    @slot('formattersData', collect($formattersData))
-    <template slot-scope="{object, onUpdate}" v-if="object">
-        <div class="title is-7 has-text-centered">
-            <span class="is-size-3" v-text="object.name || '@lang('admin/workers.createWorker')'"></span>
-        </div>
-        <dynamic-form :url="'workplace/{{$workplace->id}}/worker' + (object.id ? `/${object.id}` : '')"
-                      :on-data-update="onUpdate"
-                      :method="object.id ? 'patch' : 'post'"
-        ></dynamic-form>
-    </template>
+	@slot('deleteButton', true)
+	@slot('buttons')
+		<button class="button is-light" @click="actions.newObjectForm">@lang('vue.add')</button>
+	@endslot
+	@slot('formattersData', collect($formattersData))
+	<template slot-scope="{object, onUpdate}" v-if="object">
+		<div class="title is-7 has-text-centered">
+			<span class="is-size-3" v-text="object.name || '@lang('admin/workers.createWorker')'"></span>
+		</div>
+		<dynamic-form :url="'workplace/{{$workplace->id}}/worker' + (object.id ? `/${object.id}` : '')"
+					  :on-data-update="onUpdate"
+					  :method="object.id ? 'patch' : 'post'"
+		></dynamic-form>
+	</template>
 @endcomponent
--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,22 @@
 */
 
 
-Route::get('/','HomeController@show');
+Route::get('/', 'HomeController@show');
 
 Auth::routes([
 	'register' => false
 ]);
 
-Route::get('/language/{language}','LocaleController@set');
-Route::get('/images/{photo}','PhotoController@show');
-Route::get('/images/worker/{photo}','PhotoController@worker')->middleware(['auth','can:view,photo']);
-Route::group(['middleware' => ['auth', 'userType:' . \App\Models\Admin::class]], function (){
+Route::get('/language/{language}', 'LocaleController@set');
+Route::get('/images/{photo}', 'PhotoController@show');
+Route::get('/images/worker/{photo}', 'PhotoController@worker')->middleware(['auth', 'can:view,photo']);
+Route::group(['middleware' => ['auth', 'userType:' . \App\Models\Admin::class]], function () {
 	Route::get('datatable/list', 'DatatableController@list');
 	Route::get('datatable/export', 'DatatableController@export');
 });
-Route::get('supervisorDatatable/list', 'DatatableController@supervisorList')->middleware(['auth', 'supervisor']);
+Route::get('supervisorDatatable/{workplace}/list', 'DatatableController@supervisorList')->middleware(['auth', 'supervisor', 'can:update,workplace']);
 
 foreach (\File::allFiles(__DIR__ . "/web") as $routeFile) {
-    include $routeFile;
+	include $routeFile;
 }
 
