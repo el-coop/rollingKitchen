bb119ffa86b65d0b3671fade9aaa2098e749d0da
Nur Bar <LCD344@yahoo.com>
2019-02-28 16:59:32 -0300

    started work

---
+++ config/admin/stageTable.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'model' => \App\Models\Service::class,
+	
+	
+	'fields' => [[
+		'name' => 'id',
+		'visible' => false
+	], [
+		'name' => 'name',
+		'title' => 'global.name',
+		'sortField' => 'name',
+	]]
+];
---
+++ database/factories/StageFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Stage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
