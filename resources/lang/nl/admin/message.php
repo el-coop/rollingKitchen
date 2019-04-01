a34c9757ced2190fbfddeafdebfe770c3e27a367
Cucurrucho <adam.bar91@gmail.com>
2019-04-01 15:05:31 -0300

    send blast messages

---
+++ resources/lang/nl/admin/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'title' => 'TBT',
+	'destination' => 'TBT',
+	'subjectEn' => 'TBT',
+	'subjectNl' => 'TBT',
+	'bodyEn' => 'TBT',
+	'bodyNl' => 'TBT',
+	'channels' => 'TBT',
+	'sms' => 'TBT'
+];
