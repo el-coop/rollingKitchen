486e4cf63845896e69fad2007f59d2e768b0217f
Nur Bar <LCD344@yahoo.com>
2019-11-18 09:49:19 -0600

    Admin can now add services to kitchen application without having to go through invoicing

--- app/Http/Controllers/Admin/KitchenController.php
+++ app/Http/Controllers/Admin/KitchenController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Kitchen\DeleteKitchenRequest;
 use App\Http\Requests\Admin\Kitchen\UpdateKitchenRequest;
@@ -12,76 +13,79 @@ use App\Models\User;
 use Illuminate\Http\Request;
 
 class KitchenController extends Controller {
-	
-	public function index() {
-		$fieldType = 'Kitchen';
-		$title = __('admin/kitchens.kitchens');
-		$deleteButton = true;
-		return view('admin.datatableDefault', compact('fieldType', 'title', 'deleteButton'));
-	}
-	
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create() {
-		//
-	}
-	
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request) {
-		return true;
-	}
-	
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  \App\Models\Kitchen $kitchen
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show(Kitchen $kitchen) {
-		$kitchen->load('photos', 'user', 'applications', 'applications.products', 'applications.invoices.payments', 'applications.electricDevices', 'applications.services');
-		$indexLink = Kitchen::indexPage();
-		return view('admin.kitchens.show', compact('kitchen', 'indexLink'));
-	}
-	
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  \App\Models\Kitchen $kitchen
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit(Kitchen $kitchen) {
-		
-		return $kitchen->fullData;
-	}
-	
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  \App\Models\Kitchen $kitchen
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(UpdateKitchenRequest $request, Kitchen $kitchen) {
-		return $request->commit();
-	}
-	
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param DestroyKitchenRequest $request
-	 * @param  \App\Models\Kitchen $kitchen
-	 * @return void
-	 */
-	public function destroy(DestroyKitchenRequest $request, Kitchen $kitchen) {
-		$request->commit();
-	}
-	
+    
+    public function index() {
+        $fieldType = 'Kitchen';
+        $title = __('admin/kitchens.kitchens');
+        $deleteButton = true;
+        return view('admin.datatableDefault', compact('fieldType', 'title', 'deleteButton'));
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        //
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        return true;
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Kitchen $kitchen
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kitchen $kitchen) {
+        $locale = App::getLocale();
+        
+        $kitchen->load('photos', 'user', 'applications', 'applications.products', 'applications.invoices.payments', 'applications.electricDevices', 'applications.services');
+        $indexLink = Kitchen::indexPage();
+        $services = Service::orderByRaw("LENGTH(name_{$locale}) desc")->get();
+        return view('admin.kitchens.show', compact('kitchen', 'indexLink', 'services'));
+    }
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Kitchen $kitchen
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kitchen $kitchen) {
+        
+        return $kitchen->fullData;
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Kitchen $kitchen
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateKitchenRequest $request, Kitchen $kitchen) {
+        return $request->commit();
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param DestroyKitchenRequest $request
+     * @param \App\Models\Kitchen $kitchen
+     * @return void
+     */
+    public function destroy(DestroyKitchenRequest $request, Kitchen $kitchen) {
+        $request->commit();
+    }
+    
 }
--- resources/views/admin/kitchens/show/applications.blade.php
+++ resources/views/admin/kitchens/show/applications.blade.php
@@ -1,7 +1,7 @@
 <select-chooser :init-selected="{{ Request::input('application', 0)}}">
 	@foreach($kitchen->applications->sortByDesc('year') as $application)
 		<select-view label="{{$application->year}}">
-			@component('admin.kitchens.show.application',compact('application'))
+			@component('admin.kitchens.show.application',compact('application', 'services'))
 			@endcomponent
 		</select-view>
 	@endforeach
