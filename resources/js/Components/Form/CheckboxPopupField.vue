063c45663ee55770eebf5a7f6efea8f9892344c1
Cucurrucho <adam.bar91@gmail.com>
2020-03-10 16:57:05 -0300

    Added liability field to worker

---
+++ resources/js/Components/Form/CheckboxPopupField.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="field">
+            <label class="label" v-text="field.label" v-if="field.label && !(field.hideLabel || false)"></label>
+            <label class="checkbox">
+                <input type="checkbox" :name="field.name" :checked="field.checked" :disabled="field.disabled">
+                {{field.popup.label}} <a href="#" @click="$modal.show('popup')" v-text="field.popup.a"></a>&nbsp
+            </label>
+        </div>
+        <modal-component name="popup">
+            <div v-text="field.popup.text"></div>
+        </modal-component>
+    </div>
+</template>
+
+<script>
+    import FieldMixin from './FieldMixin';
+
+    export default {
+        name: "CheckboxPopupField",
+        mixins: [FieldMixin],
+    }
+</script>
