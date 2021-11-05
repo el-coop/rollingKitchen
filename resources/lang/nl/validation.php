c97699375950e11830f0efee22e34a287d7134e7
Cucurrucho <adam.bar91@gmail.com>
2021-11-04 22:35:08 -0300

    changed translation in dutch

--- resources/lang/nl/validation.php
+++ resources/lang/nl/validation.php
@@ -85,7 +85,7 @@ return [
 	'min' => [
 		'numeric' => ':attribute moet tenminste :min zijn.',
 		'file' => 'The :attribute must be at least :min kilobytes.',
-		'string' => 'De :attribute minimaal :min tekens lang zijn.',
+		'string' => 'De :attribute moet minimaal :min tekens lang zijn.',
 		'array' => 'The :attribute must have at least :min items.',
 	],
 	'not_in' => 'The selected :attribute is invalid.',
