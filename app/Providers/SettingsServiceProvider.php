26c263ef7cab546e342735f87f3cc7d9d8a82ea9
adam <adam.bar91@gmail.com>
2022-04-17 22:42:09 -0300

    updating check info flag on new year

--- app/Providers/SettingsServiceProvider.php
+++ app/Providers/SettingsServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Carbon\Carbon;
 use ElCoop\Valuestore\Valuestore;
 use Illuminate\Support\Facades\App;
@@ -19,6 +20,11 @@ class SettingsServiceProvider extends ServiceProvider {
 		if (today() > Carbon::create(date('Y'), 11, 14) && $this->app->settings->get('registration_year') == date('Y')) {
 			$year = date('Y') + 1;
 			$this->app->settings->put('registration_year', $year);
+            $users = User::where('checked_info', true)->get();
+            foreach ($users as $user){
+                $user->checked_info = false;
+                $user->save();
+            }
 		}
 	}
 	
