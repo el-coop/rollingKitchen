3f4917e05d00fbc78b6ed6df168c57e60e12eff4
adam <adam.bar91@gmail.com>
2022-04-17 23:42:26 -0300

    fixed translations

--- resources/lang/nl/worker/worker.php
+++ resources/lang/nl/worker/worker.php
@@ -10,7 +10,7 @@ return [
 	'taxReviews' => 'Belastingoverzicht',
 	'uploadId' => 'ID Foto',
 	'uploadInstructions' => 'Upload hier een foto van je paspoort of de voor- en achterkant van je ID kaart. Let op: een rijbewijs is niet geldig als ID.',
-    'liabilityLabel' => 'privacy policy and privacy beleid',
+    'liabilityLabel' => 'privacy beleid',
     'liabilityPopupLink' => 'algemene voorwaarden',
     'liabilityPopupLabel' => 'Ik accepteer'
 ];
