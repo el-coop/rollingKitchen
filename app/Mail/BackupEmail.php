8892c86e53ac14e3f7b4647cb9e549a8a0788382
Nur Bar <LCD344@yahoo.com>
2023-01-01 15:49:31 -0300

    updated mailer options

--- app/Mail/BackupEmail.php
+++ app/Mail/BackupEmail.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 
 class BackupEmail extends Mailable {
 	use Queueable, SerializesModels;
-	
+
 	/**
 	 * Create a new message instance.
 	 *
@@ -19,31 +19,15 @@ class BackupEmail extends Mailable {
 	public function __construct() {
 		//
 	}
-	
+
 	/**
 	 * Build the message.
 	 *
 	 * @return $this
 	 */
 	public function build() {
-		return $this->usingBackupSmtp()->from(env('BACKUP_MAIL_FROM'))->subject(env('APP_NAME') . ' backup for ' . Carbon::now()->toDateString())->view('mail.raw')->with('body', 'Backup for ' . Carbon::now()->toDateString() . ' is attached')
+		return $this->mailer('backup_smtp')->from(env('BACKUP_MAIL_FROM'))->subject(env('APP_NAME') . ' backup for ' . Carbon::now()->toDateString())->view('mail.raw')->with('body', 'Backup for ' . Carbon::now()->toDateString() . ' is attached')
 			->attach(storage_path('app/backups/backup.sql'));
 	}
-	
-	private function usingBackupSmtp() {
-		$mailTransport = app()->make('mailer')
-			->getSwiftMailer()
-			->getTransport();
-		
-		if ($mailTransport instanceof \Swift_SmtpTransport) {
-			/** @var \Swift_SmtpTransport $mailTransport */
-			$mailTransport->setHost(env('BACKUP_MAIL_HOST'));
-			$mailTransport->setPort(env('BACKUP_MAIL_PORT'));
-			$mailTransport->setEncryption(env('BACKUP_MAIL_ENCRYPTION'));
-			$mailTransport->setUsername(env('BACKUP_MAIL_USERNAME'));
-			$mailTransport->setPassword(env('BACKUP_MAIL_PASSWORD'));
-		}
-		
-		return $this;
-	}
+
 }
