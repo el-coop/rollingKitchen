3f5d6b128a57094c4fc2aa719da5f24f96447a28
Nur Bar <LCD344@yahoo.com>
2023-01-01 15:51:18 -0300

    updated mailer options

--- app/Notifications/SendAsMuzik.php
+++ app/Notifications/SendAsMuzik.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 
 trait SendAsMuzik {
     protected function usingMusicSmtp() {
-        $from = env('MAIL_BANDS_USERNAME');
+        $from = env('MAIL_BANDS_FROM_ADDRESS');
 
         return (new MailMessage)->mailer('muzik_smtp')->from($from);
     }
