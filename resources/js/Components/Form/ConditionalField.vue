af95eec4c28c2a1a4eb77eb23c1e85d539c283c3
Cucurrucho <adam.bar91@gmail.com>
2024-02-15 17:33:35 -0300

    fixed warning

--- resources/js/Components/Form/ConditionalField.vue
+++ resources/js/Components/Form/ConditionalField.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="field">
-        <label class="label" v-text="field.label"></label>
-        <div class="control">
-            <div class="select is-fullwidth" :class="{'is-danger': error}">
-                <select v-model="value" :name="field.name" :disabled="field.readonly">
-                    <option v-for="(conditionalField, key) in conditionalFields" :key="key" :value="conditionalField"
-                            v-text="conditionalField"></option>
-                </select>
+    <div>
+        <div class="field">
+            <label class="label" v-text="field.label"></label>
+            <div class="control">
+                <div class="select is-fullwidth" :class="{'is-danger': error}">
+                    <select v-model="value" :name="field.name" :disabled="field.readonly">
+                        <option v-for="(conditionalField, key) in conditionalFields" :key="key"
+                                :value="conditionalField"
+                                v-text="conditionalField"></option>
+                    </select>
+                </div>
+                <p v-if="error" class="help is-danger" v-text="errorText"></p>
             </div>
-            <p v-if="error" class="help is-danger" v-text="errorText"></p>
         </div>
-
-    </div>
-    <div class="field">
-        <div class="control">
-            <div class="select is-fullwidth" :class="{'is-danger': error}">
-                <select v-model="condition" name="condition_value">
-                    <option v-for="(condition, key) in conditions[value]" :key="key" :value="key"
-                            v-text="condition"></option>
-                </select>
+        <div class="field">
+            <div class="control">
+                <div class="select is-fullwidth" :class="{'is-danger': error}">
+                    <select v-model="condition" name="condition_value">
+                        <option v-for="(condition, key) in conditions[value]" :key="key" :value="key"
+                                v-text="condition"></option>
+                    </select>
+                </div>
+                <p v-if="error" class="help is-danger" v-text="errorText"></p>
             </div>
-            <p v-if="error" class="help is-danger" v-text="errorText"></p>
         </div>
-
     </div>
 </template>
 
@@ -35,7 +36,7 @@ export default {
     data() {
         let conditionalFields = [''];
         let conditions = {};
-        for (const [key, option] of Object.entries(this.field.options)){
+        for (const [key, option] of Object.entries(this.field.options)) {
             conditionalFields.push(option.name);
             conditions[option.name] = option.options;
         }
