df14aa435458caa972922c5444fdab4ab19a2202
Cucurrucho <adam.bar91@gmail.com>
2024-05-13 21:54:27 -0300

    added more console logs

--- resources/js/Components/Utilities/ImageManager/ImageEntry.vue
+++ resources/js/Components/Utilities/ImageManager/ImageEntry.vue
@@ -1,69 +1,65 @@
 <template>
-	<div class="list-item">
-		<div class="level is-mobile">
-			<div class="level-left">
-				<div class="level-item">
-					<span v-if="Array.isArray(image.file) && image.file.indexOf('.pdf') > 1">PDF</span>
-					<img v-else class="image is-max37-max64" :src="image.url">
-				</div>
-			</div>
-			<div class="level-right h-100">
-				<div class="level-item">
-					<button class="button is-danger is-inverted" :class="{'is-loading': deleting}" type="button"
-							@click="deleteImage">
-						<font-awesome-icon
-								icon="times-circle">
-						</font-awesome-icon>
-					</button>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="list-item">
+        <div class="level is-mobile">
+            <div class="level-left">
+                <div class="level-item">
+                    <span v-if="Array.isArray(image.file) && image.file.indexOf('.pdf') > 1">PDF</span>
+                    <img v-else class="image is-max37-max64" :src="image.url">
+                </div>
+            </div>
+            <div class="level-right h-100">
+                <div class="level-item">
+                    <button class="button is-danger is-inverted" :class="{'is-loading': deleting}" type="button"
+                            @click="deleteImage">
+                        <font-awesome-icon
+                            icon="times-circle">
+                        </font-awesome-icon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	export default {
-		name: "ImageEntry",
-		props: {
-			image: {
-				type: Object,
-				required: true
-			},
-			deleteUrl: {
-				type: String,
-				default: ''
-			},
-		},
-
-		data() {
-			return {
-				deleting: false
-			}
-		},
-        mounted() {
-            console.log(this.image);
+export default {
+    name: "ImageEntry",
+    props: {
+        image: {
+            type: Object,
+            required: true
         },
+        deleteUrl: {
+            type: String,
+            default: ''
+        },
+    },
 
-        methods: {
-			async deleteImage() {
-				this.deleting = true;
-				try {
-					axios.delete(`${this.deleteUrl}/${this.image.id}`);
-					this.$emit('deleted', this.image);
-				} catch (error) {
-					this.$toast.error(this.$translations.tryLater, this.$translations.operationFiled);
-				}
-				this.deleting = false;
-			}
-		}
-	}
+    data() {
+        return {
+            deleting: false
+        }
+    },
+    methods: {
+        async deleteImage() {
+            this.deleting = true;
+            try {
+                axios.delete(`${this.deleteUrl}/${this.image.id}`);
+                this.$emit('deleted', this.image);
+            } catch (error) {
+                this.$toast.error(this.$translations.tryLater, this.$translations.operationFiled);
+            }
+            this.deleting = false;
+        }
+    }
+}
 </script>
 
 <style scoped>
-	.image.is-max37-max64 {
-		width: auto;
-		height: auto;
-		max-width: 64px;
-		max-height: 37px;
-	}
+.image.is-max37-max64 {
+    width: auto;
+    height: auto;
+    max-width: 64px;
+    max-height: 37px;
+}
 </style>
