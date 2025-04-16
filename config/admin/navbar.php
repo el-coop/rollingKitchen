5262b62477cad8e70233b3793f2b00b36402f5f8
Cucurrucho <adam.bar91@gmail.com>
2025-04-16 19:55:58 -0300

    removed artist from navbar

--- config/admin/navbar.php
+++ config/admin/navbar.php
@@ -15,16 +15,16 @@ return [
 		'worker/worker.shifts' => 'Admin\ShiftController@index',
 		'admin/settings.workedHours' => 'Admin\WorkedHoursExportColumnController@show'
 	],
-	'admin/artists.artists' => [
-		'admin/artists.stages' => 'Admin\StageController@index',
-		'admin/artists.artistManager' => 'Admin\ArtistManagerController@index',
-		'admin/artists.bands' => 'Admin\BandController@index',
-		'admin/artists.schedule' => 'Admin\BandController@schedule',
-		'admin/bands.bandPayments' => 'Admin\BandPaymentExportColumnController@show',
-		'admin/bandMembers.exportBandMembersTitle' => 'Admin\BandMemberExportColumnController@show'
-
-
-	],
+//	'admin/artists.artists' => [
+//		'admin/artists.stages' => 'Admin\StageController@index',
+//		'admin/artists.artistManager' => 'Admin\ArtistManagerController@index',
+//		'admin/artists.bands' => 'Admin\BandController@index',
+//		'admin/artists.schedule' => 'Admin\BandController@schedule',
+//		'admin/bands.bandPayments' => 'Admin\BandPaymentExportColumnController@show',
+//		'admin/bandMembers.exportBandMembersTitle' => 'Admin\BandMemberExportColumnController@show'
+//
+//
+//	],
 	'admin/settings.title' => [
 		'admin/settings.settings' => 'Admin\SettingsController@show',
 		'admin/settings.files' => 'Admin\PDFController@index',
