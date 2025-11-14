2b00b1fab9f4458fd9df722059aebb354024049e
Cucurrucho <adam.bar91@gmail.com>
2025-11-14 20:58:10 -0300

    small translations fixxes

--- resources/lang/en/kitchen/dimensions.php
+++ resources/lang/en/kitchen/dimensions.php
@@ -2,10 +2,10 @@
 
 return [
 	'dimensions' => 'Dimensions',
-	'length' => 'Kitchen Length',
-	'width' => 'Kitchen Width',
-	'terraceLength' => 'Terrace Length',
-	'terraceWidth' => 'Terrace Width',
+	'length' => 'Length of the vehicle',
+	'width' => 'Width of the vehicle',
+	'terraceLength' => 'Length of the terrace',
+	'terraceWidth' => 'Width of the terrace',
 	'terraceSeats' => 'Terrace Seats',
 	'inMeters' => 'In meters',
 	'terraceSeatsPlaceholder' => 'benches, standing tables, picnic benches, etc',
