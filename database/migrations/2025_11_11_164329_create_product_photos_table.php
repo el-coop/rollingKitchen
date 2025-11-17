0aae26260de5d1e28f76b25543c614fa90e3b1c3
Cucurrucho <adam.bar91@gmail.com>
2025-11-17 19:37:05 -0300

    drop table that doesnt work

--- database/migrations/2025_11_11_164329_create_product_photos_table.php
+++ database/migrations/2025_11_11_164329_create_product_photos_table.php
@@ -17,7 +17,8 @@ return new class extends Migration {
 
             $table->foreign('product_id')
                 ->references('id')->on('products')
-                ->onDelete('cascade');        });
+                ->onDelete('cascade');
+        });
     }
 
     /**
